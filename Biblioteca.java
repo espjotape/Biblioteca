a874b39bf3ccc6c6e775d3c76578bf69825fd7f7
João Pedro <joaope.espindola2005@gmail.com>
2025-02-26 15:16:43 -0300

    Functionality Implementation, Add Book, List and Borrow

---
+++ Biblioteca.java
@@ -0,0 +1,35 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca {
+ private List<Livro> livros = new ArrayList<>();
+ //private List<Autor> autores = new ArrayList<>();
+ private List<Emprestimo> emprestimos = new ArrayList<>();
+
+ //Método de Adicionar Livros
+ public void adicionarLivro(Livro livro) {
+  livros.add(livro);
+ }
+
+ //Método de Listar todos os livros disponíveis
+ public void listarLivrosDisponiveis() {
+  for(Livro livro : livros) {
+   if(livro.isDisponivel()) {
+    System.out.println(livro.getId() + " - " + livro.getTitulo());
+   }
+  }
+ }
+
+ //Método de Realizar empréstimo de um livro
+  public void emprestarLivro(Livro livro, String nomeUsuario) {
+   if(livro.isDisponivel()) {
+    LocalDate dataEmprestimo = LocalDate.now();
+    Emprestimo novoEmprestimo = new Emprestimo(livro, nomeUsuario, dataEmprestimo);
+    emprestimos.add(novoEmprestimo);
+    livro.setDisponivel(false);
+   }else {
+    System.out.println("Livro não disponível para empréstimo.");
+   }
+  }
+ }
